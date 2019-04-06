1b97f4a38b5aad00badbc837f99e2a2c4c1a30aa
Nitin Kumar <nk525888@gmail.com>
2019-04-06 15:24:43 +0530

    Changed the route to create a new blog

--- routes/blog.js
+++ routes/blog.js
@@ -5,6 +5,6 @@ const AuthHelper = require('../helpers/AuthHelper');
 const BlogCntrl = require('../controllers/blog');
 
 router.get('/blogs', AuthHelper.VerifyToken, BlogCntrl.GetAllBlogs);
-router.post('/new-blog', AuthHelper.VerifyToken, BlogCntrl.CreateNewBlog);
+router.post('/create-new-blog', AuthHelper.VerifyToken, BlogCntrl.CreateNewBlog);
 
 module.exports = router;
